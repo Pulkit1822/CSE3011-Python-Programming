d1a2dafc12d192834aeea9404d682f49cee58118
Pulkit1822 <pulkitkumarmathur1@gmail.com>
2024-03-08 11:07:05 +0530

    classwork

---
+++ Classwork/codingClass1.py
@@ -0,0 +1,31 @@
+# VARIABLES AND DATA TYPES
+
+
+# x = 10
+# y = x
+# print (x, y)
+# print (type(x), type(y))
+# print (id(x),id(y))
+
+
+#operators
+
+#1. remainder operator
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: ")) 
+# print(a % b)
+
+
+#2. relational operations
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: ")) 
+# print(a > b)
+
+
+#3. logical operations
+a = float(input("Enter a: "))
+b = float(input("Enter b: ")) 
+c = float(input("Enter c: "))
+print("a and b: ",(a and b))
+print("a or b: ",(a or b))
+print("a and b or c: ",(a and b or c))
