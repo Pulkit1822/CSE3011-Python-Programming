69d6633c8bf96d184c79ccbcee23e9bccc7247a7
Pulkit1822 <pulkitkumarmathur1@gmail.com>
2024-03-09 09:29:16 +0530

    Refactored logical operations and added new sections for bitwise, shifting, and identity operations

--- Classwork/codingClass1.py
+++ Classwork/codingClass1.py
@@ -23,9 +23,34 @@
 
 
 #3. logical operations
-a = float(input("Enter a: "))
-b = float(input("Enter b: ")) 
-c = float(input("Enter c: "))
-print("a and b: ",(a and b))
-print("a or b: ",(a or b))
-print("a and b or c: ",(a and b or c))
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: ")) 
+# c = float(input("Enter c: "))
+# print("a and b: ",(a and b))
+# print("a or b: ",(a or b))
+# print("a and b or c: ",(a and b or c))
+
+
+# 4. Bitwise operations
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: ")) 
+# print("a and b: ",(a & b))
+# print("a or b: ",(a | ~b))
+
+
+# 5. Shifting operations
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: ")) 
+# print("b>>1: ",(b>>1))
+# print("b<<1: ",(b<<1))
+# print("b<<2: ",(b<<2))
+
+
+# 6. Identity operations
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = a 
+print("a is b: ",(a is b))
+print("a is c: ",(a is c))
+print("a == b: ",(a == b))
+print("a == c: ",(a == c))
