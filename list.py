57cdb3f4c08662377618b852c654bb8b6da30885
Pulkit1822 <pulkitkumarmathur1@gmail.com>
2024-04-01 23:19:00 +0530

    Fix bug in login functionality

---
+++ list.py
@@ -0,0 +1,31 @@
+courses = ['maths', 'science', 'english', 'history', 'geography']
+# courses.insert(1, 'chemistry')
+
+
+# courses2 = ['art', 'music']
+# courses.extend(courses2)
+# courses.remove('maths')
+# print(courses)
+
+
+
+
+# marks = [120, 32, 13, 45, 29]
+# print(min(marks))
+
+
+
+# for i in courses:
+#     print(i)
+    
+"""
+party is on 🚀
+"""  
+    
+for index, course in enumerate(courses, start=1):
+    print(index, course)
+
+
+
+
+
